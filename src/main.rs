385bfeb029420d4bfea900a322b9e0a12413caa9
Ruu <3532340532@qq.com>
2025-05-02 08:03:55 +0800

    feat:添加进程检测

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod defs;
+mod framework;
+
+use std::fs;
+
+use anyhow::{Context, Result};
+
+fn check() -> Result<()> {
+    let procs = procfs::process::all_processes().context("无法获取进程列表")?;
+    let self_name = fs::read_to_string("/proc/self/comm")?;
+
+    for proc in procs {
+        let proc = proc.context("无法获取进程信息")?;
+        if let Ok(states) = proc.status() {
+            if states.name.contains(self_name.trim()) && states.pid != std::process::id() as i32 {
+                log::error!("发现其他进程,pid:{},即将退出", states.pid);
+                std::process::exit(1);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    if cfg!(debug_assertions) {
+        simple_logger::SimpleLogger::new()
+            .with_local_timestamps()
+            .with_level(log::LevelFilter::Debug)
+            .init()?;
+    } else {
+        simple_logger::SimpleLogger::new()
+            .with_local_timestamps()
+            .with_level(log::LevelFilter::Info)
+            .init()?;
+    }
+    check()?;
+    log::info!("life-death-scheduler正在启动");
+    crate::framework::scheduler::Looper::new().init();
+    Ok(())
 }
