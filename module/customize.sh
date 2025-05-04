042c643b4bd19f03b8f85c291eee84ba8cd8f45f
Ruu <3532340532@qq.com>
2025-05-04 17:48:10 +0800

    fix:修复权限问题

--- module/customize.sh
+++ module/customize.sh
@@ -3,5 +3,11 @@ SKIPUNZIP=0
 
 ui_print "欢迎使用life-death-scheduler"
 ui_print "此调度适用于大部分设备"
-
 ui_print "有问题请到687235389反馈"
+
+sleep 0.3
+
+ui_print "正在设置权限"
+
+set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm $MODPATH/life-death-scheduler 0 0 0755
