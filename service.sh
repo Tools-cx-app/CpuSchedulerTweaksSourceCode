e919eecc3312c673b1078472b0362c6f8f6add07
Ruu <3532340532@qq.com>
2025-05-25 20:25:45 +0800

    feat:添加检查/data是否挂载，如果挂载，运行进程

--- service.sh
+++ service.sh
@@ -3,6 +3,10 @@
 # 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
 MODDIR=${0%/*}
 
+until [ -d "$MODDIR" ]; do
+	sleep 1
+done
+
 RUST_BACKTRACE=1 nohup $MODDIR/life-death-scheduler >$MODDIR/run.log 2>&1 &
 # 这个脚本将以 late_start service 模式执行
 # 更多信息请访问 Magisk 主题
