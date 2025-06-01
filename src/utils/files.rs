34cb90d69bebc42ac5556f6f4d9daee199ee899e
Ruu <3532340532@qq.com>
2025-06-01 19:09:10 +0800

    feat:添加写入时的debug日志

--- src/utils/files.rs
+++ src/utils/files.rs
@@ -11,6 +11,7 @@ pub fn write_with_locked<T: AsRef<Path>>(path: T, content: &str) -> Result<()> {
     set_permissions(&path, Permissions::from_mode(0o644))
         .context(format!("无法设置644权限:{}", path.display()))?;
     write(&path, content).context(format!("无法写入{}:{}", content, path.display()))?;
+    log::debug!("已写入{}到{}", content, path.display());
     set_permissions(&path, Permissions::from_mode(0o400))
         .context(format!("无法设置400权限:{}", path.display()))?;
     Ok(())
