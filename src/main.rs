4c02ee78f1d1e95e3d98b5905379d55cb2130bd7
Ruu <3532340532@qq.com>
2025-06-02 14:49:31 +0800

    This reverts commit c3fdf64765271f1c5f9ae157f3ebdc79d18b1cfa.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,10 @@ mod defs;
 mod framework;
 mod utils;
 
-use std::{
-    fs::{self, OpenOptions},
-    io::{Read, Write},
-};
+use std::{fs, io::Write};
 
-use anyhow::{Context, Result, anyhow};
-use defs::MOD_PROP_PATH;
+use anyhow::{Context, Result};
 use env_logger::Builder;
-use libc::{STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO, close, fork, kill, setsid, umask};
-use regex::Regex;
 
 fn check() -> Result<()> {
     let procs = procfs::process::all_processes().context("无法获取进程列表")?;
@@ -47,94 +41,9 @@ fn init_logger() -> Result<()> {
     builder.filter_level(log::LevelFilter::Info).init();
     Ok(())
 }
-
-fn daemon() -> Result<()> {
-    unsafe {
-        match fork() {
-            -1 => return Err(anyhow!("fork进程失败")),
-            0 => {}
-            _ => std::process::exit(0),
-        }
-
-        if setsid() == -1 {
-            return Err(anyhow!("setsid失败"));
-        }
-
-        match fork() {
-            -1 => return Err(anyhow!("fork进程失败")),
-            0 => {}
-            _ => std::process::exit(0),
-        }
-
-        umask(0);
-    }
-    Ok(())
-}
-
-fn check_pid(pid: i32, sig: i32) -> bool {
-    unsafe { kill(pid, sig) != 0 }
-}
-
-fn create_daemon() {
-    let pid = std::process::id() as i32;
-
-    match unsafe { fork() } {
-        -1 => {
-            eprintln!("fork失败");
-            std::process::exit(-2);
-        }
-        0 => {
-            if let Err(e) = daemon() {
-                log::error!("daemon启动失败");
-                std::process::exit(-3);
-            }
-            let mut file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .open(MOD_PROP_PATH)
-                .unwrap();
-            loop {
-                if check_pid(pid, 0) {
-                    let mut buf =String::new();
-                    file.read_to_string(&mut buf);
-                    let re = Regex::new(r"(?m)^(description\s*=\s*).*$").unwrap();
-
-                    file.write(
-                        re.replace_all(&buf.clone(), |caps: &regex::Captures| {
-                            format!(
-                                "{}{}",
-                                &caps[1], "[运行状态: 未运行] 适用于大部分设备的CPU动态调速器"
-                            )
-                        })
-                        .as_bytes(),
-                    );
-
-                    std::process::exit(-4);
-                } else {
-                    let mut buf = String::new();
-                    file.read_to_string(&mut buf);
-                    let re = Regex::new(r"(?m)^(description\s*=\s*).*$").unwrap();
-
-                    file.write(
-                        re.replace_all(&buf.clone(), |caps: &regex::Captures| {
-                            format!(
-                                "{}{}",
-                                &caps[1], "[运行状态: 运行中] 适用于大部分设备的CPU动态调速器"
-                            )
-                        })
-                        .as_bytes(),
-                    );
-                }
-            }
-        }
-        _ => {}
-    }
-}
-
 fn main() -> Result<()> {
     init_logger().context("初始化日志加载器失败")?;
     check()?;
-    create_daemon();
     let _ = fs::write(
         "/dev/cpuset/background/cgroup.procs",
         std::process::id().to_string(),
