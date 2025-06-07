bc064c31b8fb4073565818a050b34cc6a8ea0e06
Tools-app <3532340532@qq.com>
2025-06-07 12:33:11 +0800

    feat:优化cpu频率写入部分

--- src/framework/scheduler/cpu/freq.rs
+++ src/framework/scheduler/cpu/freq.rs
@@ -1,16 +1,10 @@
-use std::{
-    path::{Path, PathBuf},
-    sync::atomic::Ordering,
-};
-
-use anyhow::Result;
-
+use super::{ClusterPaths, Cpu};
 use crate::{
     framework::scheduler::{DEBUG, Mode},
     utils::{files::write_with_locked, option_to_str},
 };
-
-use super::Cpu;
+use anyhow::Result;
+use std::{path::Path, sync::atomic::Ordering};
 
 pub trait CpuFreqs {
     fn set_freq(&self, mode: Mode);
@@ -19,210 +13,124 @@ pub trait CpuFreqs {
 
 impl CpuFreqs for Cpu {
     /*
-     * cpu频率设置
-     * 2025-05-24
+     * Cpu频率写入
+     * 2025-06-07
      */
     fn set_freq(&self, mode: Mode) {
-        let big_path = self
-            .config
-            .cpu_config
-            .big
-            .map(|id| PathBuf::from(format!("/sys/devices/system/cpu/cpufreq/policy{id}")));
-        let big = big_path.as_ref().map(|p| p.as_path());
-
-        let middle_path = self
-            .config
-            .cpu_config
-            .middle
-            .map(|id| PathBuf::from(format!("/sys/devices/system/cpu/cpufreq/policy{id}")));
-        let middle = middle_path.as_ref().map(|p| p.as_path());
-
-        let small_path = self
-            .config
-            .cpu_config
-            .small
-            .map(|id| PathBuf::from(format!("/sys/devices/system/cpu/cpufreq/policy{id}")));
-        let small = small_path.as_ref().map(|p| p.as_path());
-
-        let super_big_path = self
-            .config
-            .cpu_config
-            .super_big
-            .map(|id| PathBuf::from(format!("/sys/devices/system/cpu/cpufreq/policy{id}")));
-        let super_big = super_big_path.as_ref().map(|p| p.as_path());
-
-        let has_big = big.map(|p| p.exists()).unwrap_or(false);
-        let has_middle = middle.map(|p| p.exists()).unwrap_or(false);
-        let has_small_big = small.map(|p| p.exists()).unwrap_or(false);
-        let has_super_big = super_big.map(|p| p.exists()).unwrap_or(false);
+        let clusters = self.get_cluster_paths();
+        let (has_big, has_middle, has_small, has_super_big) = clusters.check_existence();
 
         if DEBUG.load(Ordering::Relaxed) {
-            if let Some(b) = big {
-                log::debug!("big簇: {}", b.display());
-            }
-            if let Some(m) = middle {
-                log::debug!("middle簇: {}", m.display());
-            }
-            if let Some(s) = small {
-                log::debug!("small簇: {}", s.display());
-            }
-            if let Some(sb) = super_big {
-                log::debug!("super_big簇: {}", sb.display());
-            }
+            clusters.debug_print();
         }
 
-        let mut big_freq = Vec::new();
-        let mut middle_freq = Vec::new();
-        let mut small_freq = Vec::new();
-        let mut super_big_freq = Vec::new();
-
-        /*
-         * 自动判断，并按顺序写入频率变量
-         * 2025-05-24
-         */
-        match mode {
-            Mode::Powersave => {
-                if has_big {
-                    big_freq.extend(&[
-                        option_to_str(self.config.powersave.freqs.big_cpu).max,
+        let get_freqs = |cluster: &str| {
+            let (min, max) = match mode {
+                Mode::Powersave => match cluster {
+                    "big" => (
                         option_to_str(self.config.powersave.freqs.big_cpu).min,
-                    ]);
-                }
-                if has_middle {
-                    middle_freq.extend(&[
-                        option_to_str(self.config.powersave.freqs.middle_cpu).max,
+                        option_to_str(self.config.powersave.freqs.big_cpu).max,
+                    ),
+                    "middle" => (
                         option_to_str(self.config.powersave.freqs.middle_cpu).min,
-                    ]);
-                }
-                if has_small_big {
-                    small_freq.extend(&[
-                        option_to_str(self.config.powersave.freqs.small_cpu).max,
+                        option_to_str(self.config.powersave.freqs.middle_cpu).max,
+                    ),
+                    "small" => (
                         option_to_str(self.config.powersave.freqs.small_cpu).min,
-                    ]);
-                }
-                if has_super_big {
-                    super_big_freq.extend(&[
-                        option_to_str(self.config.powersave.freqs.super_big_cpu).max,
+                        option_to_str(self.config.powersave.freqs.small_cpu).max,
+                    ),
+                    "super_big" => (
                         option_to_str(self.config.powersave.freqs.super_big_cpu).min,
-                    ]);
-                }
-            }
-            Mode::Balance => {
-                if has_big {
-                    big_freq.extend(&[
-                        option_to_str(self.config.balance.freqs.big_cpu).max,
+                        option_to_str(self.config.powersave.freqs.super_big_cpu).max,
+                    ),
+                    _ => (0, 0),
+                },
+                Mode::Balance => match cluster {
+                    "big" => (
                         option_to_str(self.config.balance.freqs.big_cpu).min,
-                    ]);
-                }
-                if has_middle {
-                    middle_freq.extend(&[
-                        option_to_str(self.config.balance.freqs.middle_cpu).max,
+                        option_to_str(self.config.balance.freqs.big_cpu).max,
+                    ),
+                    "middle" => (
                         option_to_str(self.config.balance.freqs.middle_cpu).min,
-                    ]);
-                }
-                if has_small_big {
-                    small_freq.extend(&[
-                        option_to_str(self.config.balance.freqs.small_cpu).max,
+                        option_to_str(self.config.balance.freqs.middle_cpu).max,
+                    ),
+                    "small" => (
                         option_to_str(self.config.balance.freqs.small_cpu).min,
-                    ]);
-                }
-                if has_super_big {
-                    super_big_freq.extend(&[
-                        option_to_str(self.config.balance.freqs.super_big_cpu).max,
+                        option_to_str(self.config.balance.freqs.small_cpu).max,
+                    ),
+                    "super_big" => (
                         option_to_str(self.config.balance.freqs.super_big_cpu).min,
-                    ]);
-                }
-            }
-            Mode::Performance => {
-                if has_big {
-                    big_freq.extend(&[
-                        option_to_str(self.config.performance.freqs.big_cpu).max,
+                        option_to_str(self.config.balance.freqs.super_big_cpu).max,
+                    ),
+                    _ => (0, 0),
+                },
+                Mode::Performance => match cluster {
+                    "big" => (
                         option_to_str(self.config.performance.freqs.big_cpu).min,
-                    ]);
-                }
-                if has_middle {
-                    middle_freq.extend(&[
-                        option_to_str(self.config.performance.freqs.middle_cpu).max,
+                        option_to_str(self.config.performance.freqs.big_cpu).max,
+                    ),
+                    "middle" => (
                         option_to_str(self.config.performance.freqs.middle_cpu).min,
-                    ]);
-                }
-                if has_small_big {
-                    small_freq.extend(&[
-                        option_to_str(self.config.performance.freqs.small_cpu).max,
+                        option_to_str(self.config.performance.freqs.middle_cpu).max,
+                    ),
+                    "small" => (
                         option_to_str(self.config.performance.freqs.small_cpu).min,
-                    ]);
-                }
-                if has_super_big {
-                    super_big_freq.extend(&[
-                        option_to_str(self.config.performance.freqs.super_big_cpu).max,
+                        option_to_str(self.config.performance.freqs.small_cpu).max,
+                    ),
+                    "super_big" => (
                         option_to_str(self.config.performance.freqs.super_big_cpu).min,
-                    ]);
-                }
-            }
-            Mode::Fast => {
-                if has_big {
-                    big_freq.extend(&[
-                        option_to_str(self.config.fast.freqs.big_cpu).max,
+                        option_to_str(self.config.performance.freqs.super_big_cpu).max,
+                    ),
+                    _ => (0, 0),
+                },
+                Mode::Fast => match cluster {
+                    "big" => (
                         option_to_str(self.config.fast.freqs.big_cpu).min,
-                    ]);
-                }
-                if has_middle {
-                    middle_freq.extend(&[
-                        option_to_str(self.config.fast.freqs.middle_cpu).max,
+                        option_to_str(self.config.fast.freqs.big_cpu).max,
+                    ),
+                    "middle" => (
                         option_to_str(self.config.fast.freqs.middle_cpu).min,
-                    ]);
-                }
-                if has_small_big {
-                    small_freq.extend(&[
-                        option_to_str(self.config.fast.freqs.small_cpu).max,
+                        option_to_str(self.config.fast.freqs.middle_cpu).max,
+                    ),
+                    "small" => (
                         option_to_str(self.config.fast.freqs.small_cpu).min,
-                    ]);
-                }
-                if has_super_big {
-                    super_big_freq.extend(&[
-                        option_to_str(self.config.fast.freqs.super_big_cpu).max,
+                        option_to_str(self.config.fast.freqs.small_cpu).max,
+                    ),
+                    "super_big" => (
                         option_to_str(self.config.fast.freqs.super_big_cpu).min,
-                    ]);
-                }
-            }
-        }
+                        option_to_str(self.config.fast.freqs.super_big_cpu).max,
+                    ),
+                    _ => (0, 0),
+                },
+            };
+            vec![max, min]
+        };
 
-        if has_big {
-            if let Some(b) = big {
-                let _ = self.write_freq(b, big_freq);
-            }
+        if has_big && let Some(path) = &clusters.big {
+            let _ = self.write_freq(Path::new(path), get_freqs("big"));
         }
-        if has_middle {
-            if let Some(m) = middle {
-                let _ = self.write_freq(m, middle_freq);
-            }
+        if has_middle && let Some(path) = &clusters.middle {
+            let _ = self.write_freq(Path::new(path), get_freqs("middle"));
         }
-        if has_small_big {
-            if let Some(s) = small {
-                let _ = self.write_freq(s, small_freq);
-            }
+        if has_small && let Some(path) = &clusters.small {
+            let _ = self.write_freq(Path::new(path), get_freqs("small"));
         }
-        if has_super_big {
-            if let Some(sb) = super_big {
-                let _ = self.write_freq(sb, super_big_freq);
-            }
+        if has_super_big && let Some(path) = &clusters.super_big {
+            let _ = self.write_freq(Path::new(path), get_freqs("super_big"));
         }
     }
 
-    /*
-     * 频率写入部分
-     * 2025-05-24
-     */
     fn write_freq(&self, path: &Path, freq: Vec<u64>) -> Result<()> {
-        let max = path.join("/scaling_max_freq");
-        let min = path.join("/scaling_min_freq");
-
         if freq.len() < 2 {
             anyhow::bail!("无效的频率参数，需要最大和最小频率");
         }
 
-        let _ = write_with_locked(max, freq[0].to_string().as_str());
-        let _ = write_with_locked(min, freq[1].to_string().as_str());
+        let max = path.join("scaling_max_freq");
+        let min = path.join("scaling_min_freq");
+
+        write_with_locked(&max, &freq[0].to_string())?;
+        write_with_locked(&min, &freq[1].to_string())?;
         Ok(())
     }
 }
+
