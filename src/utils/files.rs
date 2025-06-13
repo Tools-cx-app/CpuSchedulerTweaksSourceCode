2c4a9947556dc40bd4931524e844709284a1f860
Tools-app <3532340532@qq.com>
2025-06-13 19:52:18 +0800

    feat:回溯bc064c31b8fb4073565818a050b34cc6a8ea0e06的write_with_lock函数

--- src/utils/files.rs
+++ src/utils/files.rs
@@ -9,17 +9,11 @@ use anyhow::{Context, Result};
 
 pub fn write_with_locked<T: AsRef<Path>>(path: T, content: &str) -> Result<()> {
     let path = path.as_ref();
-    Command::new("chattr")
-        .args(["+i", &path.to_string_lossy()])
-        .output()?;
     set_permissions(&path, Permissions::from_mode(0o644))
         .context(format!("无法设置644权限:{}", path.display()))?;
     write(&path, content).context(format!("无法写入{}:{}", content, path.display()))?;
     log::debug!("已写入{}到{}", content, path.display());
     set_permissions(&path, Permissions::from_mode(0o400))
         .context(format!("无法设置400权限:{}", path.display()))?;
-    Command::new("chattr")
-        .args(["-i", &path.to_string_lossy()])
-        .output()?;
     Ok(())
 }
