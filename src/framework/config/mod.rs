dcb45b177e4b7bf194ead16a6723ba5494f4de32
Tools-app <3532340532@qq.com>
2025-06-23 06:35:36 +0800

    feat: 优化配置文件重载
    Signed-off-by: Tools-app <3532340532@qq.com>

--- src/framework/config/mod.rs
+++ src/framework/config/mod.rs
@@ -8,121 +8,29 @@ pub mod data;
 
 impl data::ConfigData {
     pub fn new() -> Self {
-        let s = Self {
-            powersave: CpuInfo {
-                freqs: CpuFreqs {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                governor: Governor {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                cpuctl: CpuCtl {
-                    top_app: CpuCtlInfo {
-                        shares: 0,
-                        uclamp: CpuCtlUclamp { max: 0, min: 0 },
-                    },
-                },
-            },
-            balance: CpuInfo {
-                freqs: CpuFreqs {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                governor: Governor {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                cpuctl: CpuCtl {
-                    top_app: CpuCtlInfo {
-                        shares: 0,
-                        uclamp: CpuCtlUclamp { max: 0, min: 0 },
-                    },
-                },
-            },
-            performance: CpuInfo {
-                freqs: CpuFreqs {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                governor: Governor {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                cpuctl: CpuCtl {
-                    top_app: CpuCtlInfo {
-                        shares: 0,
-                        uclamp: CpuCtlUclamp { max: 0, min: 0 },
-                    },
-                },
-            },
-            fast: CpuInfo {
-                freqs: CpuFreqs {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                governor: Governor {
-                    super_big_cpu: None,
-                    big_cpu: None,
-                    middle_cpu: None,
-                    small_cpu: None,
-                },
-                cpuctl: CpuCtl {
-                    top_app: CpuCtlInfo {
-                        shares: 0,
-                        uclamp: CpuCtlUclamp { max: 0, min: 0 },
-                    },
-                },
-            },
-            cpu_config: data::Cpu {
-                super_big: None,
-                big: None,
-                middle: None,
-                small: None,
-            },
-            io: Io {
-                scheduler: String::new(),
-                read_ahead: 0,
-            },
-            app_launch_boost: true,
-            osm: String::new(),
-            applist: HashMap::new(),
-            debug: false,
-        };
-        s.load_config()
+        load_config()
     }
 
     pub fn load_config(&self) -> Self {
-        if let Err(e) = fs::read_to_string(defs::CONFIG_PATH) {
-            log::error!("无法读取配置文件:{},请检查{}", e, defs::CONFIG_PATH);
-        }
-        let content = fs::read_to_string(defs::CONFIG_PATH).unwrap();
-        let toml: Self = match toml::from_str(content.as_str()) {
-            Ok(s) => s,
-            Err(e) => {
-                log::error!("配置文件错误:{}", e);
-                std::process::exit(2);
-            }
-        };
-        toml
+        load_config()
     }
 
     pub fn is_emtpy(&self) -> bool {
         self.osm.is_empty()
     }
+}
+
+pub fn load_config() -> data::ConfigData {
+    if let Err(e) = fs::read_to_string(defs::CONFIG_PATH) {
+        log::error!("无法读取配置文件:{},请检查{}", e, defs::CONFIG_PATH);
+    }
+    let content = fs::read_to_string(defs::CONFIG_PATH).unwrap();
+    let toml: data::ConfigData = match toml::from_str(content.as_str()) {
+        Ok(s) => s,
+        Err(e) => {
+            log::error!("配置文件错误:{}", e);
+            std::process::exit(2);
+        }
+    };
+    toml
 }
